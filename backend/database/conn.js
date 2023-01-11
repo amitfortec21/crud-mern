1e05f5398a59df073ae4c166e852bde1e8fbc4c5
Amit Sharma <fortec.developer21@gmail.com>
2023-01-11 13:57:57 +0530

    added .env file

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,6 +1,7 @@
 import { mongoose } from "mongoose";
+
 // const uri = "mongodb://127.0.0.1:27017/crud";
-const uri = "mongodb+srv://amit244245:amit2442455@merncrudcluster.7jzjbti.mongodb.net/mern_crud";
+const uri = "mongodb+srv://amit244245:amit2442455@merncrudcluster.7jzjbti.mongodb.net/mern_crud"
 
 mongoose.set("strictQuery", false);
 
